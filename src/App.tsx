1e3b08ea44b5d99e13ef365f242ee7c1f655767e
ivanov6444 <a.ivanov6444@gmail.com>
2023-07-11 10:19:29 +0300

    feat: base routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Link} from 'react-router-dom';
+import { HomePage } from "./pages/Homepage";
+import { AboutPage } from "./pages/Aboutpage";
+import { BlogPage } from "./pages/Blogpage";
+import { NotFoundPage } from "./pages/Notfoundpage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/posts">Blog</Link>
+        <Link to="/about">About</Link>
       </header>
-    </div>
+      <Routes>
+          <Route path="/" element={ <HomePage /> } />
+          <Route path="/about" element={ <AboutPage /> } />
+          <Route path="/posts" element={ <BlogPage /> } />
+          <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
